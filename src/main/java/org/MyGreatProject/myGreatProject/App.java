fd99d85d35078086108c3627a5801e446a81c7bc
Robin Fabre <rf.school@icloud.com>
2016-05-31 11:33:46 +0200

    gitignore update + main

---
+++ src/main/java/org/MyGreatProject/myGreatProject/App.java
@@ -0,0 +1,13 @@
+package org.MyGreatProject.myGreatProject;
+
+import com.google.common.base.Strings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println(Strings.isNullOrEmpty(""));
+	}
+}
